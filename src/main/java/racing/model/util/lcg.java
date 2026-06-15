bdbefc183b7a9373934beb0233f828eeff6ec9fb
jakobgraetz <96118933+jakobgraetz@users.noreply.github.com>
2026-06-15 22:38:29 +0200

    implement utility methods

--- src/main/java/racing/model/util/lcg.java
+++ src/main/java/racing/model/util/lcg.java
@@ -27,7 +27,7 @@ public class LCG {
         // This will be in int range and is good until 18 Jan 2038.
         // Date.getTime() returns a long by default: until 18th of January 2038, dividing by 1000 is
         // enough to cast this into an int.
-        this.seed = int i = (int) (new Date().getTime() / 1000);
+        this.seed = (int) (new Date().getTime() / 1000);
         this.modulus = (1 << 16) + 1;
         this.multiplier = 75;
         this.increment = 0;
@@ -40,20 +40,25 @@ public class LCG {
     // can provide modular arithmetic by storage-bit truncation."
     // (https://en.wikipedia.org/wiki/Linear_congruential_generator, 30th of May 2026, 10.31 CEST)
     // lcg(seed, modulus m, multiplier a, increment c)
-    public void lcg(
-        int seed,
-        int m,
-        int a,
-        int c,
-        int[] randomNumbers,
-        int numberOfRandomNumbers
-    ) {
+    public void lcg(int[] randomNumbers, int numberOfRandomNumbers) {
         randomNumbers[0] = this.seed;
 
         for (int i = 1; i < numberOfRandomNumbers; i++) {
-            randomNumbers[i] = ((randomNumbers[i - 1] * a) + c) % m;
+            randomNumbers[i] =
+                ((randomNumbers[i - 1] * this.multiplier) + this.increment) %
+                this.modulus;
         }
     }
 
-    public void randomNumbers()
+    public int[] randomNumbers(int[] arr, int n) {
+        lcg(arr, n);
+        return arr;
+    }
+
+    public int randomNumber() {
+        int[] arr;
+        arr = new int[1];
+
+        return randomNumbers(arr, 1)[0];
+    }
 }
